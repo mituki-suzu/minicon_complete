d788fc789b53ea50fa3a9ba156ebf7dad3c03478
Mituki Suzu <22077477+mituki-suzu@users.noreply.github.com>
2018-02-16 07:23:35 +0000

    サイドバーカテゴライズ追加

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,14 @@ class EventsController < ApplicationController
   def index
     @events = Event.page(params[:page]).per(3)
     
-    @events = Event.get_by_title(params[:title]).page(params[:page]).per(3)  if params[:title].present?
+    @events = Event.get_by_id(params[:id]).page(params[:page]).per(3) if params[:id].present?
+    
+    @categories = EventCategory.all
+    
+    # @events = Event.get_by_id(params[:koikatu]).page(params[:page]).per(3) if params[:koikatu].present?
+    
+    # @events = Event.get_by_id(params[:gourmet]).page(params[:page]).per(3) if params[:gourmet].present?
+    
+    @events = Event.get_by_title(params[:title]).page(params[:page]).per(3) if params[:title].present?
   end
 end
